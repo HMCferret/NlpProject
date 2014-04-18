12573967eb7cb45dfc3e7079dea0f4c400562b29
Tu Nguyen <hikaruyh@gmail.com>
2014-04-18 19:28:03 -0400

    add Demo() in Main function

--- NlpProject/src/Main.java
+++ NlpProject/src/Main.java
@@ -1,12 +1,14 @@
 import java.io.FileNotFoundException;
 import java.io.PrintStream;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.Vector;
 
+import weka.core.Instances;
 import edu.stanford.nlp.io.IOUtils;
 import edu.stanford.nlp.ling.CoreAnnotations;
 import edu.stanford.nlp.pipeline.Annotation;
@@ -48,5 +50,45 @@ public class Main {
 		    	Quote q= it.next();
 		    	System.out.println(++cnt + ":" + "[" + q.speaker.name + "]" + "=>" + q.text);
 		    }
+		    
+		   Demo();
+	}
+	
+	public static void Demo() throws Exception
+	{
+		List<CandidateQuote> cq = new ArrayList<CandidateQuote>();
+	    CandidateQuote item = new CandidateQuote();
+	    
+	    item.distance = 2;
+	    item.commaPresence = true;
+	    item.periodPresence = false;
+	    item.punctuationPresence = true;
+	    item.ordinal = 2;
+	    item.proportion = 35.0;
+	    item.numNameCd = 2;
+	    item.numQuoteCd = 3;
+	    item.numWordCd = 4;
+	    item.numNameQt = 4;
+	    item.numQuoteQt = 3;
+	    item.numWordQt = 7;
+	    item.numNameOtr = 9;
+	    item.numQuoteOtr = 8;
+	    item.numWordOtr = 35;
+	    item.numAppearance = 12;
+	    item.expVerbPresence = false;
+	    item.personPresence = true;
+	    item.quoteLength = 37;
+	    item.quotePosition = 22;
+	    item.otherCharPresence = true;
+	    item.candidatePresence = false;
+	    
+	    cq.add(item);
+	    
+	    AttributeJ48 att = new AttributeJ48(cq,200,2);
+	    att.Create();
+	    att.AddData();
+	    
+	    Instances data = att.ShowData();
+	    System.out.println(data);
 	}
 }
