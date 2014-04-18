cef895209e3026c9b7f332eb61ca91c9e92e171c
Tu Nguyen <hikaruyh@gmail.com>
2014-04-18 19:24:13 -0400

    2 tu

---
+++ NlpProject/src/CandidateQuote.java
@@ -0,0 +1,43 @@
+
+public class CandidateQuote {
+	//1
+	public int distance;
+	
+	//2
+	public boolean commaPresence;
+	public boolean periodPresence;
+	public boolean punctuationPresence;
+	
+	//3
+	public int ordinal;
+	
+	//4
+	public double proportion;
+	
+	//5
+	public int numNameCd;
+	public int numQuoteCd;
+	public int numWordCd;
+	
+	public int numNameQt;
+	public int numQuoteQt;
+	public int numWordQt;
+	
+	public int numNameOtr;
+	public int numQuoteOtr;
+	public int numWordOtr;
+	
+	//6
+	public int numAppearance;
+	
+	//7
+	public boolean expVerbPresence;
+	public boolean personPresence;
+	
+	
+	//8
+	public int quoteLength;
+	public int quotePosition;
+	public boolean otherCharPresence;
+	public boolean candidatePresence;
+}
