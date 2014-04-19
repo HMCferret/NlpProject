4a2b363448778fec7fb85acf4d7e5be2f8c445d0
Tu Nguyen <hikaruyh@gmail.com>
2014-04-19 00:52:57 -0400

    add SNFeatureExtraction

---
+++ NlpProject/src/CandidateQuoteResult.java
@@ -0,0 +1,10 @@
+
+public class CandidateQuoteResult {
+	 public CandidateQuote cq;
+     public boolean candidateIsSpeaker;
+     
+     public CandidateQuoteResult( CandidateQuote cq, boolean candidateIsSpeaker){
+    	 this.cq=cq;
+    	 this.candidateIsSpeaker =candidateIsSpeaker; 
+     }
+}
