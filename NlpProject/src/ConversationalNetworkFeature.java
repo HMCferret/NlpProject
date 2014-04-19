9c812ddf0a7a0424915a0449c5788719d7bc9685
Tu Nguyen <hikaruyh@gmail.com>
2014-04-19 01:44:54 -0400

    a

--- NlpProject/src/ConversationalNetworkFeature.java
+++ NlpProject/src/ConversationalNetworkFeature.java
@@ -1,4 +1,25 @@
 
 public class ConversationalNetworkFeature {
-
+	//1
+	public int numChar;
+	public int numSpeakingChar;
+	
+	//2
+	public double firstMostFqtChar;
+	public double secondMostFqtChar;
+	public double thirdMostFqtChar;
+	
+	//3
+	public int numQuote;
+	public double quoteProportion;
+	
+	//4
+	public int num3Cliche;
+	public int num4Cliche;
+	
+	//5
+	public double avgDegree;
+	
+	//6
+	public double graphDensity;
 }
